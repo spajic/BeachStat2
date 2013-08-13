f79fa2c12f4ce514b94532a5e7318a13472c59a9
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-13 14:34:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeachStat2::Application.config.secret_key_base = '5be7d73e186ed81be3ea2a4005e361cba15bc7e988f3ca55395aae5b6a706ac9ff9743c454639f262d370bb5d436d6d2849037c265df637a37a9ca8f043b12fd'
