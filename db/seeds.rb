361d4e04f51b0e082771f06498fb6229b55e8946
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-13 16:54:02 -0700

    Add players of first day in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Players = ['Спаич', 'Прас', 'Баранов', 'Олег', 'Стёпа', 'Федоринин']
+
+Players.each {|p| Player.find_or_create_by_name(p)}
