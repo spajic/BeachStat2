126a48e35b776ce6f1b496db6dcc30aae4d642bd
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-14 10:48:09 -0700

    games index tests and passed

--- spec/requests/game_pages_spec.rb
+++ spec/requests/game_pages_spec.rb
@@ -4,19 +4,35 @@ describe "Game Pages" do
   subject { page }
 
   describe "index page" do
-    before { visit games_path }
+  	fixtures :days
+  	fixtures :players
+  	fixtures :games
+  	fixtures :results
+
+    before { 
+    	visit games_path 
+    	save_and_open_page
+    }
 
     it { should have_content('Результаты всех игр') }
 
     describe 'should show table column names' do
-		it { should have_content('День') }
-    	it { should have_content('№') }
-    	it { should have_content('Winner 1') }
-    	it { should have_content('Winner 2') }
-    	it { should have_content('Loser 1') }
-		it { should have_content('Loser 2') }
-    	it { should have_content('W pts') }
-    	it { should have_content('L pts') }
+		it { 
+			should have_content('День') 
+			should have_content('№')
+			should have_content('Winner 1')
+			should have_content('Winner 2')
+			should have_content('Loser 1')
+			should have_content('Loser 2')
+			should have_content('W pts')
+			should have_content('L pts')
+		}
+    	
     end
+
+    describe 'shold show data' do
+    	it { should have_content('Спаич') }
+    end
+
   end
 end
