1a7bfd83b731794c59c3acf596333c51331df21f
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-14 04:55:36 -0700

    Add Day

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813230117) do
+ActiveRecord::Schema.define(version: 20130814114928) do
+
+  create_table "days", force: true do |t|
+    t.date     "date"
+    t.string   "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "players", force: true do |t|
     t.string   "name"
