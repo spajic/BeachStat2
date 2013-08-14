1e532cb18fa3c1e08814a5fa9aaf2455adbf2127
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-14 05:48:15 -0700

    add day 1 to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Players = ['Спаич', 'Прас', 'Баранов', 'Олег', 'Стёпа', 'Федоринин']
-
 Players.each {|p| Player.find_or_create_by_name(p)}
+
+# День 1 (id = 1)
+d1 = Day.find_or_create_by_id(id:1)
+d1.date = '13.05.2013'.to_date
+d1.comment = 
+	"Очень тёплая погода. 
+	Тепло, хорошо. Не самый плохой уровени волейбола для открытия сезона"
+d1.save
+
