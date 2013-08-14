2d7c7749e3ade986c2e3ad1418111e094fd6afa3
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-14 07:50:59 -0700

    generate Games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BeachStat2::Application.routes.draw do
+  get "games/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
