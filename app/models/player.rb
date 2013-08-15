bc7de8489132be62699eaba94b76fd3524e959c6
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-15 15:36:59 -0700

    second part of methods for Player;

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,10 @@
 class Player < ActiveRecord::Base
 	has_many :results
 
+	def Player.load_data_for_rating_calculation
+		@@players = Player.all.to_a
+	end
+
 	def wins_count
 		results.where(win: true).count
 	end
@@ -13,15 +17,19 @@ class Player < ActiveRecord::Base
 	def wins_to_games_ratio
 		(100 * (wins_count.to_f / games_count)).round
 	end
-	def more_manly_man_count
-		
+	def more_wins_man_count
+		@@players.count {|p| p.wins_count > wins_count}
 	end
-	def more_manly_man_rating
+	def more_wins_man_ratio
+		( 100 * (1 - more_wins_man_count.to_f / Player.count ) ).round
 	end
 	def rating
+		(wins_to_games_ratio + more_wins_man_ratio) / 2
 	end
 	def more_rating_man_count
+		@@players.count {|p| p.rating > rating}
 	end
 	def place
+		1 + more_rating_man_count
 	end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -10,12 +10,13 @@ describe Player do
     before  { @player = Player.new }
     subject { @player }
 
+    it { Player.should respond_to(:load_data_for_rating_calculation) }
 	it { should respond_to(:wins_count				) }
 	it { should respond_to(:loses_count				) }
 	it { should respond_to(:games_count				) }
 	it { should respond_to(:wins_to_games_ratio		) }
-	it { should respond_to(:more_manly_man_count	) }
-	it { should respond_to(:more_manly_man_rating	) }
+	it { should respond_to(:more_wins_man_count		) }
+	it { should respond_to(:more_wins_man_ratio		) }
 	it { should respond_to(:rating 					) }
 	it { should respond_to(:more_rating_man_count 	) }
 	it { should respond_to(:place 					) }
@@ -23,6 +24,7 @@ describe Player do
 
   describe 'it work correctly on fixtures data' do
   	before  { 
+  		Player.load_data_for_rating_calculation
   		@spajic = Player.first 
   		@pras   = Player.find(2)
   	}
@@ -36,7 +38,19 @@ describe Player do
   	it { @spajic.wins_to_games_ratio.should eq(100) }
  	it { @pras  .wins_to_games_ratio.should eq(50) }
 
- 	#it { @spajic.more_manly_man_count.should eq(0) }
-	#it { @pras  .more_manly_man_count.should eq(1) }
+ 	it { @spajic.more_wins_man_count.should eq(0) }
+	it { @pras  .more_wins_man_count.should eq(1) }
+
+	it { @spajic.more_wins_man_ratio.should eq(100) }
+	it { @pras.more_wins_man_ratio.should   eq(75) }
+
+	it { @spajic.rating.should eq(100) }
+	it { @pras.rating.should eq(62) }
+
+	it { @spajic.more_rating_man_count.should eq(0) }
+	it { @pras  .more_rating_man_count.should eq(1) }
+
+	it { @spajic.place.should eq(1) }
+	it { @pras  .place.should eq(2) }
   end
 end
