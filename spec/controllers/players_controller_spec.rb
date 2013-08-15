53f8302a30172e1f628c626174c820dc848a5589
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-15 07:46:39 -0700

    teste for players index controller

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -3,10 +3,29 @@ require 'spec_helper'
 describe PlayersController do
 
   describe "GET 'index'" do
+    it 'should call Player.all method' do
+    	Player.should_receive(:all)
+    	get :index
+    end
+
+    it 'should select Players templste for rendering' do
+    	Player.stub(:all) # Заменяем метод пустышкой для изоляции
+    	get :index
+    	response.should render_template('index')
+    end
+
+    it 'should make Player.all results available to template' do
+    	fake_results = [ mock('Player') ]
+    	Player.stub(:all).and_return(fake_results)
+    	get :index
+    	assigns(:players).should == fake_results
+    end
+
     it "returns http success" do
       get 'index'
       response.should be_success
     end
+
   end
 
 end
