145a6e4c72b9615d3ac6b932d06b8b9a6afe377d
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-15 14:48:08 -0700

    first bunch of methods for Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,27 @@
 class Player < ActiveRecord::Base
 	has_many :results
+
+	def wins_count
+		results.where(win: true).count
+	end
+	def loses_count
+		results.where(win: false).count
+	end
+	def games_count
+		results.count
+	end
+	def wins_to_games_ratio
+		(100 * (wins_count.to_f / games_count)).round
+	end
+	def more_manly_man_count
+		
+	end
+	def more_manly_man_rating
+	end
+	def rating
+	end
+	def more_rating_man_count
+	end
+	def place
+	end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe Player do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :players
+  fixtures :days
+  fixtures :games 
+  fixtures :results
+  
+  describe 'it should respond to methods' do
+    before  { @player = Player.new }
+    subject { @player }
+
+	it { should respond_to(:wins_count				) }
+	it { should respond_to(:loses_count				) }
+	it { should respond_to(:games_count				) }
+	it { should respond_to(:wins_to_games_ratio		) }
+	it { should respond_to(:more_manly_man_count	) }
+	it { should respond_to(:more_manly_man_rating	) }
+	it { should respond_to(:rating 					) }
+	it { should respond_to(:more_rating_man_count 	) }
+	it { should respond_to(:place 					) }
+  end
+
+  describe 'it work correctly on fixtures data' do
+  	before  { 
+  		@spajic = Player.first 
+  		@pras   = Player.find(2)
+  	}
+  	it { @spajic.name.should eq('Спаич') }
+  	it { @pras  .name.should eq('Прас' ) }
+
+  	it { @spajic.wins_count.should eq(2) }
+  	it { @spajic.loses_count.should eq(0) }
+  	it { @spajic.games_count.should eq(2) }
+
+  	it { @spajic.wins_to_games_ratio.should eq(100) }
+ 	it { @pras  .wins_to_games_ratio.should eq(50) }
+
+ 	#it { @spajic.more_manly_man_count.should eq(0) }
+	#it { @pras  .more_manly_man_count.should eq(1) }
+  end
 end
