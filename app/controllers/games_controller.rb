e5040af7916ceaad3749802761b1508413747a19
Alexey Vasilyev <bestspajic@gmail.com>
2013-08-17 14:34:03 -0700

    load games.players info with helpers, reduce sql count and time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,12 @@
 class GamesController < ApplicationController
   def index
-  	@games = Game.all
+  	# eager loading - Изначально было Game.all и 919 sql запросов
+  	# .includes(:day) => 896 sql
+  	# .includes(:day, :results) => 896 sql
+  	# .includes(:day, results: [:player]) => 896 sql
+  	# helper для получения first_winner => 676 sql
+  	# helper's для получения всех игроков => 5 sql
+  	# Время загрузки сократилось с 12 секунд до 1 секунды
+  	@games = Game.includes(:day, results: [:player]).all
   end
 end
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,19 @@
 module GamesHelper
+	# На входе массив из 4х результатов - два победителя и два проигравших
+	def first_winner(game_results)
+		winners = game_results.select{|r| r.win == true}
+		winners.first
+	end
+	def second_winner(game_results)
+		winners = game_results.select{|r| r.win == true}
+		winners.last
+	end
+	def first_loser(game_results)
+		losers = game_results.select{|r| r.win == false}
+		losers.first
+	end
+	def second_loser(game_results)
+		losers = game_results.select{|r| r.win == false}
+		losers.last
+	end
 end
