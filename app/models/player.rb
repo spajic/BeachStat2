ef90d23499074332024251604d368ec906be76f0
Alexey Vasilyev <bestspajic@gmail.com>
2015-07-21 07:12:47 -0700

    (!) add caching through instance variables and dramatically speed up

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,7 @@ class Player < ActiveRecord::Base
 	has_many :games, through: :results
 	has_many :days, through: :games
 	@@players = Player.includes(:results, :games, :days).all.to_a
+	@@players_count = Player.count
 
 	def Player.legioners
 		Player.where(is_legioner: true).sort!{|p1,p2| p2.days_count <=> p1.days_count}	
@@ -11,34 +12,34 @@ class Player < ActiveRecord::Base
 		Player.legioners.collect{|player| player.days_count}.reduce(:+)*200
 	end
 	def days_count
-		results.joins(game: :day).select(:date).distinct.count
+		@days ||= days.distinct.count
 	end
 	def wins_count
-		results.where(win: true).count
+		@wins ||= results.where(win: true).count
 	end
 	def loses_count
-		results.where(win: false).count
+		@loses ||= results.where(win: false).count
 	end
 	def games_count
-		results.count
+		@results_count ||= results.count
 	end
 	def wins_to_games_ratio
-		(100 * (wins_count.to_f / games_count)).round
+		@wins_to_games_ratio ||= (100 * (wins_count.to_f / games_count)).round
 	end
 	def more_wins_man_count
-		@@players.count {|p| p.wins_count > wins_count}
+		@more_wins_man_count ||= @@players.count {|p| p.wins_count > wins_count}
 	end
 	def more_wins_man_ratio
-		( 100 * (1 - more_wins_man_count.to_f / (Player.count-1) ) ).round
+		@more_wind_man_ratio ||= ( 100 * (1 - more_wins_man_count.to_f / (@@players_count-1) ) ).round
 	end
 	def rating
-		(wins_to_games_ratio + more_wins_man_ratio) / 2
+		@rating ||=	(wins_to_games_ratio + more_wins_man_ratio) / 2
 	end
 	def more_rating_man_count
-		@@players.count {|p| p.rating > rating}
+		@more_rating_man_count ||=	@@players.count {|p| p.rating > rating}
 	end
 	def place
-		1 + more_rating_man_count
+		@place ||= 1 + more_rating_man_count
 	end
 
 	def Player.all_sorted_by_place
